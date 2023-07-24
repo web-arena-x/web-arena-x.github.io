04a7797886e4df1636accb00ad59ceca010a4350
Frank Xu <frankxu2004@gmail.com>
2023-07-24 02:49:34 -0400

    Update

--- static/js/index.js
+++ static/js/index.js
@@ -31,11 +31,12 @@ $(document).ready(function() {
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 3,
+			slidesToShow: 2,
 			loop: true,
 			infinite: true,
 			autoplay: false,
 			autoplaySpeed: 3000,
+      pagination: false
     }
 
 		// Initialize all div with carousel class
